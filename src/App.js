3caff01671e95dc9b04f9f79b80d687db707b8f5
TolulopeBabajide <babajidetoluwalope@gmail.com>
2023-11-06 13:09:48 +0000

    Home,About,Profile,Signup and SignUp pages and routes created

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './SignUp'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+      <Route element={<Home/>} path="/" />
+      <Route element={<About/>} path="/about" />
+      <Route element={<Profile/>} path="/profile" />
+      <Route element={<SignIn/>} path="/signIn" />
+      <Route element={<SignUp/>} path="/signUp" />
+        
+      </Routes>
+    </BrowserRouter>
   )
 }
 
