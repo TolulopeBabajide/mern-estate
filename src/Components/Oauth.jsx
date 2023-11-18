edce97f9c4a9e61f96a607b9190f6fe06a68a19f
TolulopeBabajide <babajidetoluwalope@gmail.com>
2023-11-18 12:30:00 +0000

    Oauth functionality added to Oauth

--- src/Components/Oauth.jsx
+++ src/Components/Oauth.jsx
@@ -1,28 +1,54 @@
 import React from 'react'
 import {GoogleAuthProvider, getAuth, signInWithPopup} from 'firebase/auth';
 import {app} from '../firebase';
+import { useDispatch } from 'react-redux';
+import {useNavigate} from 'react-router-dom'
+import { signInSuccess } from '../redux/user/userSlice';
 
 
 
-const Oauth = () => {
+export default function Oauth(){
 
-  const handleGoogleClick =async ()=>{
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleGoogleClick = async()=>{
     try {
 
       const provider= new GoogleAuthProvider();
       const auth = getAuth(app)
 
       const result = await signInWithPopup(auth,provider);
-      console.log(result);
-      
+    
+      const res = await fetch('http://localhost:3000/api/auth/google',{
+        method:'POST',
+        headers:{
+          "Content-Type":"application/json"
+        }, 
+        body:JSON.stringify({
+          name: result.user.displayName,
+          email:result.user.email, 
+          photo:result.user.photoURL}),
+        credentials: 'include',
+
+      });
+        console.log(result)
+      const data=  await res.json();
+      dispatch(signInSuccess(data));
+      navigate('/home');
+
+
     } catch (error) {
       console.log("Could not sign in with google", error);
     }
   };
 
   return (
-    <button type="button" onClick={handleGoogleClick} className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>Continue with google</button>
+    <button 
+      type="button" 
+      onClick={handleGoogleClick} 
+      className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>
+        Continue with google
+    </button>
   )
 }
 
-export default Oauth
